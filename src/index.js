091c5d9f8661c473e9c40d8863e3d1d005ff5fff
cambeckingsale <cambeckingsale@gmail.com>
2021-10-27 22:00:28 +1100

    got a working css up for further dev. needs lots of work to be nice UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import controller from './modules/controller';
+import css from './style.css';
 
 document.addEventListener('DOMContentLoaded', controller.onLoad);
--- src/modules/DOMcontroller.js
+++ src/modules/DOMcontroller.js
@@ -1,8 +1,19 @@
 import DOMproject from './DOMproject';
 
+const renderTitleBar = () => {
+  const titleBar = document.createElement('div');
+  titleBar.classList.add('title-bar');
+  const title = document.createElement('h1');
+  title.classList.add('title');
+  title.innerText = 'DODO LIST';
+  titleBar.appendChild(title);
+  return titleBar;
+};
+
 const renderHomePage = (projects) => {
   // create project element
   const content = document.querySelector('#content');
+  content.appendChild(renderTitleBar());
   console.log(projects);
   projects.forEach((project) => {
     content.appendChild(DOMproject.renderProjectElement(project));
